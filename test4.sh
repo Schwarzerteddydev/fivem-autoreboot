f01baa8f071163d431f1ca9b404908087703104f
Schwarzer_teddy <69168996+Schwarzerteddydev@users.noreply.github.com>
2022-09-24 14:21:15 +0200

    Update and rename test3.sh to test4.sh

--- test4.sh
+++ test4.sh
@@ -29,6 +29,6 @@ sudo systemctl daemon-reload
 sudo systemctl start fivem
 sudo systemctl enable fivem
 echo 'Permissions set for fivem_start.sh fivem.service to 777'
-sudo chmod +x /usr/bin/fivem_start.sh
+sudo chmod 777 /usr/bin/fivem_start.sh
 chmod 777 /lib/systemd/system/fivem.service
 echo 'set permissions'
